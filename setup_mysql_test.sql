2e753f5202fa81ef92095c5346a977458f5b9196
KwameSei <nathanielosei92@gmail.com>
2022-12-07 19:08:45 +0000

    Changes made to set up file

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,11 @@
 -- setting up MySQL enviroment
+-- creating database name
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd_20#ONE';
-GRANT USAGE ON *.* TO 'hbnb_test'@'localhost'
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- creating new user named, hbnb_dev with password hbnb_dev_pwd
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- granting the new user created all privileges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+-- granting the select privilege to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
