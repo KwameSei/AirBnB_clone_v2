b2b05546d2c2db614fc22a7a51e5d1b4fa06cdac
KwameSei <nathanielosei92@gmail.com>
2022-12-07 16:12:19 +0000

    Changes made to setup_mysql_dev.sql file

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,12 @@
 -- setting up MySQL enviroment
+-- creating database name
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost'
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+-- creating new user named, hbnb_dev with password hbnb_dev_pwd
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd_20#ONE';
+-- granting the new user created all privileges
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+-- granting the select privilege to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
