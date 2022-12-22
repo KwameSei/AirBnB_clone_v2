e729f94015787939873d2718337bc59c5756e085
KwameSei <nathanielosei92@gmail.com>
2022-12-22 05:46:59 +0000

    Fabric script that distributes an archive to web servers updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,39 +6,23 @@
 import os
 from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
-
+from fabric.decorator import runs_once
 
 env.hosts = ['52.86.124.17', '18.234.80.128']
 env.user = "ubuntu"
 
 
-def do_pack(archive_path):
-    """Distributes  an archive to a web server.
-        Args:
-            archive_path (str): The path of the archive to distribute.
-        Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+@runs_once
+def do_pack():
+    """Generating .tgz archive from the contents of a folder"""
+    local("mkdir -p versions")
+    date = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+    path = ("versions/web_static_{}.tgz".format(date))
+    output = local("tar -cvzf {} web_static".format(path))
+
+    if output.failed:
+        return None
+    return path
 
 
 def do_deploy(archive_path):
