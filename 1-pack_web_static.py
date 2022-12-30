e70d6bc0072caf45a84a5cd821691a3825a74e00
KwameSei <nathanielosei92@gmail.com>
2022-12-30 20:55:56 +0000

    generating a .tgz archive from the contents of web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,32 @@
-#!/usr/bin/python3
+#!/usr/local/lib/python3
 """Fabric script that generates a .tgz archive
     from the contents of the web_static folder
 """
 
 from fabric.api import local
 from datetime import datetime
-from fabric.decorators import runs_once
 
 
-@runs_once
 def do_pack():
     """Generating .tgz archive from the contents of a folder"""
-    local("mkdir -p versions")
-    date = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
-    path = ("versions/web_static_{}.tgz".format(date))
-    output = local("tar -cvzf {} web_static".format(path))
+    folder = 'web_static'
+    # Create the versions folder if it doesn't exist
+    new_dir = local('mkdir -p versions')
+    new_dir = 'versions'
 
-    if out.failed:
-        return None
-    return path
+    # Get the current date and time
+    date = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+    filename = "{}_{}.tgz".format(folder, date)
+
+    path = ("{}/{}".format(new_dir, filename))
+    # Create the .tgz archive
+    result = local("tar -cvzf {} {}".format(path, folder))
+
+    if result.succeeded:
+        #return result
+        return path
+    #else:
+        #return None
+    # Move the .tgz archive to the "versions" folder
+    #local("mv {} {}".format(filename, new_dir))
+    return None
