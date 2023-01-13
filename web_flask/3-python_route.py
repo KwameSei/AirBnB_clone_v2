2a12caa11da6e544beb34afdf2753859c225d0be
KwameSei <nathanielosei92@gmail.com>
2023-01-13 18:04:54 +0000

    Displaying content based on url

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displaying Hello HBNB!"""
+
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_page():
+    """Displaying HBNB"""
+
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_page(text):
+    """Displaying HBNB"""
+
+    text = text.replace("_", " ")
+
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
