50769ba45143f17162e1cc715fabc2c314ec81e1
KwameSei <nathanielosei92@gmail.com>
2023-01-13 18:12:51 +0000

    Displaying content based on url edited

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -29,5 +29,15 @@ def c_page(text):
     return "C {}".format(text)
 
 
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_page(text="is cool"):
+    """Displaying content based on url"""
+
+    text = text.replace("_", " ")
+
+    return "Python {}".format(text)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
