dbb88f81c72548e71e2853c8ebe6685600ac9df3
KwameSei <nathanielosei92@gmail.com>
2023-01-14 06:10:24 +0000

    Displaying number using jinja edited

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Flask web application"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displaying Hello HBNB!"""
+
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_page():
+    """Displaying HBNB"""
+
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_page(text):
+    """Displaying HBNB"""
+
+    text = text.replace("_", " ")
+
+    return "C {}".format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_page(text="is cool"):
+    """Displaying content based on url"""
+
+    text = text.replace("_", " ")
+
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_page(n):
+    """Displaying Number"""
+
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template_page(n):
+    """Displaying Number using jinja"""
+
+    return render_template('5-number.html', num=n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
