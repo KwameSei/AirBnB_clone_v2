6136c5855bc506528e840951801321ed325ed6fe
KwameSei <nathanielosei92@gmail.com>
2023-01-16 04:34:22 +0000

    Displaying single states based on their ids edited

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -8,6 +8,68 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displaying Hello HBNB!"""
+
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_page():
+    """Displaying HBNB"""
+
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_page(text):
+    """Displaying HBNB"""
+
+    text = text.replace("_", " ")
+
+    return "C {}".format(text)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_page(text="is cool"):
+    """Displaying content based on url"""
+
+    text = text.replace("_", " ")
+
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_page(n):
+    """Displaying Number"""
+
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template_page(n):
+    """Displaying Number using jinja"""
+
+    return render_template('5-number.html', num=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def even_or_odd_page(n):
+    """Displaying Number using jinja"""
+
+    return render_template('6-number_odd_or_even.html', num=n)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """Displaying a list of all state objects in the database storage"""
+
+    states = storage.all("State")
+    return render_template('7-states_list.html', all_states=states)
+
+
 @app.route('/cities_by_states', strict_slashes=False)
 def cities():
     """Displaying a list of all city objects in the database storage"""
